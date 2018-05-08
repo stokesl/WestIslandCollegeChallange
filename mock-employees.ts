ee46926566621fbec53eec61461a95e629e21d66
stokesl <39000912+stokesl@users.noreply.github.com>
2018-05-07 22:20:47 -0700

    Delete mock-employees.ts

--- mock-employees.ts
+++
@@ -1,7 +0,0 @@
-import { Employee } from './employee';
-
-export const EMPLOYEES: Employee[] = [
-
-	{"id":"52a5098c-71e8-471d-ad1d-dd5da4f703ef","firstName":"Fred","lastName":"Flintstone","dateOfBirth":"1979-11-14T00:00:00","hobbies":["Golf","Vollyball","Dancing"],"address":{"street":"P.O. Box 787","city":"Calgary","province":"AB","postalCode":"T2V 1W1"},"imageUrl":"https://upload.wikimedia.org/wikipedia/en/a/ad/Fred_Flintstone.png","salary":56000.42,"isCanadian":true}
-
-];
