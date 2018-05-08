27bf1386f03979c7f41c1e736d3d694ab7beb806
stokesl <39000912+stokesl@users.noreply.github.com>
2018-05-07 22:19:58 -0700

    Add files via upload

--- employees/employees.component.ts
+++ employees/employees.component.ts
@@ -9,6 +9,7 @@ import { EmployeeService } from '../employee.service';
 })
 export class EmployeesComponent implements OnInit {
 
+  //Variables
 	employees: Employee[];
 	selectedEmployee: Employee;
 
@@ -18,10 +19,12 @@ export class EmployeesComponent implements OnInit {
   	this.getEmployees();
   }
 
+  //Sets the selected employee when the user selects one from the list
   onSelect(employee: Employee): void{
   	this.selectedEmployee = employee;
   }
 
+  //Retrieves the list of employees
   getEmployees(): void{
   	this.employeeService.getEmployees().subscribe(employees => this.employees = employees);
   }
